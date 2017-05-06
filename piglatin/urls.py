89eda7d4ee84a522f98316037d79d758c0a76a16
Francisco Echeverria <fcontreras18@gmail.com>
2017-05-05 17:11:22 -0700

    Finish translator

--- piglatin/urls.py
+++ piglatin/urls.py
@@ -20,5 +20,6 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/', views.hello)
+    url(r'^$', views.home, name='home'),
+    url(r'^translate/', views.translate, name='translate')
 ]
--- piglatin/views.py
+++ piglatin/views.py
@@ -2,5 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def hello(request):
+def home(request):
     return render(request, 'home.html')
+
+
+def translate(request):
+    original = request.GET['original-text'].lower()
+
+    translation = ''
+    for word in original.split():
+        if word[0] in ['a','e','i','o','u']:
+            translation += word
+            translation += 'way '
+        else:
+            translation += word[1:]
+            translation += word[0]
+            translation += 'ay ' 
+
+    return render(request, 'translate.html', {'original':original, 
+                                              'translation':translation})
